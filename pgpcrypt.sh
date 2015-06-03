ab3b8aeb37382702b36fd2fe0b9c1ec80c206cff
jared <jared@jaredvm2.virutal.lab>
2015-06-03 13:55:16 -0400

    adding pgpcrypt.sh

---
+++ pgpcrypt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$2" ]
+  then 
+    echo [!] Command usage: pgpcrypt.sh dest_user_email_address message_in_quotes
+    exit
+fi
+
+#check if we have the public key
+gpg --list-public-keys $1 > /dev/null 2>&1
+
+if [ $? -ne 0 ]
+ then
+    gpg --search-keys $1
+    gpg --list-public-keys $1 > /dev/null 2>&1
+    if [ $? -ne 0 ]
+      then
+        echo [!] Can not find a public key for $1 on this computer. Aborting..
+        exit
+    fi
+fi
+
+echo $2 | gpg --encrypt --recipient $1 --sign --armour
+
